9b94797e0354540a9511af0e2d47df9616a5966f
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-13 15:55:43 +0300

    feat: done basic api

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express')
 require('dotenv').config()
 require("./src/db/dbConnection")
+const router = require('./src/routers/index');
 
 const app = express()
 const port = process.env.PORT || 5002
+
+// Middlewares
+app.use(express.json())
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({limit: "50mb", extended:true, parameterLimit:50000}))
+
+app.use("/api", router)
 
 app.get("/", (req, res) => {
     res.json({
         message: "Hoş geldiniz."
     })
 })
-
-const router = require('./src/routers/index');
-
-app.use("/api", router)
 
 app.listen(port, () => {
     console.log(`Server ${port} portunda çalışıyor...`)
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,10 +1,48 @@
+const user = require("../models/user.model");
+const bcrypt = require("bcrypt");
+
 const login = async (req, res) => {
-    console.log(req.body);
-}
+  console.log(req.body);
+  return res.json(req.body);
+};
 
 const register = async (req, res) => {
-    console.log(req.body);
-}
+  const { email } = req.body;
+  const userCheck = await user.findOne({ email });
 
+  if (userCheck) {
+    console.log("Girmiş olduğunuz mail kullanılmda", email);
+  }
 
-module.exports = {login, register}
+  req.body.password = await bcrypt.hash(req.body.password, 10);
+
+  console.log("hash şifre: ", req.body.password);
+
+  try {
+    const userSave = new user(req.body);
+
+    await userSave
+      .save()
+      .then((response) => {
+        // Burada 'response' adını kullandık
+        return res.status(201).json({
+          success: true,
+          data: response,
+          message: "Kayıt başarıyla yapıldı",
+        });
+      })
+      .catch((err) => {
+        console.log("Hata var: ", err);
+        return res.status(500).json({
+          success: false,
+          message: "Kayıt işlemi sırasında bir hata oluştu",
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(req.body);
+};
+
+module.exports = { login, register };
