bab0f68a98470a42c5515e581778f58ccd280313
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-13 19:24:50 +0300

    feat: created response utils

---
+++ src/utils/response.js
@@ -0,0 +1,67 @@
+class Response {
+
+    constructor(data = null, message = null, status) {
+        this.data = data
+        this.message = message
+        this.status = status
+    }
+
+    success(res) {
+        return res.status(200).json({
+            success: true,
+            data: this.data,
+            message : this.message ?? "İşlem başarılı",
+        })
+    }
+
+    created(res) {
+        return res.status(201).json({
+            success: true,
+            data: this.data,
+            message : this.message ?? "İşlem başarılı",
+        })
+    }
+
+    error500(res) {
+        return res.status(500).json({
+            success: false,
+            data: this.data,
+            message : this.message ?? "İşlem başarısız",
+        })
+    }
+
+    error400(res) {
+        return res.status(400).json({
+            success: false,
+            data: this.data,
+            message : this.message ?? "İşlem başarısız",
+        })
+    }
+
+    error401(res) {
+        return res.status(401).json({
+            success: false,
+            data: this.data,
+            message : this.message ?? "Lütfen oturum açın",
+        })
+    }
+
+    error404(res) {
+        return res.status(404).json({
+            success: false,
+            data: this.data,
+            message : this.message ?? "İşlem başarısız",
+        })
+    }
+
+    error429(res) {
+        return res.status(429).json({
+            success: false,
+            data: this.data,
+            message : this.message ?? "Çok fazla istek",
+        })
+    }
+
+}
+
+module.exports = Response
