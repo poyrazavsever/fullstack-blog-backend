dea1c0178e59b7d116814e824f8ec982158e6e22
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-13 23:18:55 +0300

    feat: added login validation

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {login, register} = require('../controller/auth.controller')
 const authValidatin = require('../middlewares/validations/auth.validation')
 
-router.post('/login', login)
+router.post('/login', authValidatin.login, login)
 
 router.post('/register', authValidatin.register, register)
 
