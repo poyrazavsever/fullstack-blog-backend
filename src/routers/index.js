abe3b996e95c545480798309a51051a5d3f58d2e
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-15 22:37:54 +0300

    feat: done categories

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const auth = require('./auth.routes');
 const post = require('./post.routes')
+const categories = require('./category.routes')
 
 router.use(auth)
 router.use(post)
+router.use(categories)
 
 module.exports = router
