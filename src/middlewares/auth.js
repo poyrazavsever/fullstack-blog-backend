a9ef7ea839c0eca1dd3c13dccb241c189275b84e
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-19 12:23:27 +0300

    feat: update payload to creatToken

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,51 +1,55 @@
 const user = require("../models/user.model");
-const jwt = require('jsonwebtoken');
-const APIError = require('../utils/errors');
+const jwt = require("jsonwebtoken");
+const APIError = require("../utils/errors");
 
 const createToken = async (user, res) => {
-    console.log(user);
+  console.log(user);
 
-    const payload = {
-        sub: user._id,
-        name: user.name
-    }
+  const payload = {
+    sub: user._id,
+    name: user.name,
+    lastname: user.lastname,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  };
 
-    const token = await jwt.sign(payload, process.env.JWT_SECRET_KEY, {
-        algorithm: "HS512",
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+  const token = await jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    algorithm: "HS512",
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-
-    return res.status(201).json({
-        success: true, 
-        token,
-        message: "Başarılı"
-    });
-}
+  return res.status(201).json({
+    success: true,
+    token,
+    message: "Başarılı",
+  });
+};
 
 const tokenCheck = async (req, res, next) => {
-    const headerToken = req.headers.authorization && req.headers.authorization.startsWith("Bearer ") 
+  const headerToken =
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ");
 
-    if (!headerToken)
-        throw new APIError("Geçersiz Oturum Lütfen Oturum Açın",401)
+  if (!headerToken)
+    throw new APIError("Geçersiz Oturum Lütfen Oturum Açın", 401);
 
-    const token = req.headers.authorization.split(" ")[1]
+  const token = req.headers.authorization.split(" ")[1];
 
-    await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-        if (err) throw new APIError("Geçersiz Token",401)
+  await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+    if (err) throw new APIError("Geçersiz Token", 401);
 
-        const userInfo = await user.findById(decoded.sub).select("_id name lastname email ")
+    const userInfo = await user
+      .findById(decoded.sub)
+      .select("_id name lastname email ");
 
-        if (!userInfo)
-            throw new APIError("Geçersiz Token",401)
+    if (!userInfo) throw new APIError("Geçersiz Token", 401);
 
-        req.user = userInfo
-        next();
-    })
-    
-}
+    req.user = userInfo;
+    next();
+  });
+};
 
 module.exports = {
-    createToken,
-    tokenCheck
-}
+  createToken,
+  tokenCheck,
+};
