c394760e50b1b26e9521ef1e021830cfe8a55785
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-10-19 10:53:13 +0300

    feat: added getAllUser controller and route

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -2,23 +2,23 @@ const user = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const APIError = require("../utils/errors");
 const Response = require("../utils/response");
-const {createToken} = require("../middlewares/auth")
+const { createToken } = require("../middlewares/auth");
 
 const login = async (req, res) => {
-  const {email, password} = req.body
-  
-  const userInfo = await user.findOne({email})
+  const { email, password } = req.body;
 
-  if(!userInfo) {
-    throw new APIError('Böyle bir kullanıcı bulunmamaktadır.', 401)
+  const userInfo = await user.findOne({ email });
+
+  if (!userInfo) {
+    throw new APIError("Böyle bir kullanıcı bulunmamaktadır.", 401);
   }
 
-  const comparePassword = await bcrypt.compare(password, userInfo.password)
-  
-  if(!comparePassword)
-    throw new APIError('Şifreniz hatalıdır.', 401)
-  
-  createToken(userInfo, res)
+  const comparePassword = await bcrypt.compare(password, userInfo.password);
+
+  if (!comparePassword)
+    throw new APIError("Şifreniz hatalıdır.", 401);
+
+  createToken(userInfo, res);
 };
 
 const register = async (req, res) => {
@@ -26,7 +26,7 @@ const register = async (req, res) => {
   const userCheck = await user.findOne({ email });
 
   if (userCheck) {
-    throw new APIError("Girmiş olduğunuz mail kullanılmda", 401);
+    throw new APIError("Girmiş olduğunuz mail kullanılmakta", 401);
   }
 
   req.body.password = await bcrypt.hash(req.body.password, 10);
@@ -41,15 +41,26 @@ const register = async (req, res) => {
       return new Response(data, "Kayıt başarıyla yapıldı").created(res);
     })
     .catch((err) => {
-      throw new APIError("Kullanıcı kayıt edilirken bir hatayla karşılandı!", 400)
+      throw new APIError(
+        "Kullanıcı kayıt edilirken bir hatayla karşılandı!",
+        400
+      );
     });
 };
 
 const me = async (req, res) => {
   console.log("me fonksiyonu içersinde");
-  console.log(req.user)
-  return new Response(req.user).success(res)
-  
-}
+  console.log(req.user);
+  return new Response(req.user).success(res);
+};
 
-module.exports = { login, register, me };
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await user.find();
+    return new Response(users, "Tüm kullanıcılar başarıyla getirildi").success(res);
+  } catch (err) {
+    throw new APIError("Kullanıcılar getirilirken bir hata oluştu", 500);
+  }
+};
+
+module.exports = { login, register, me, getAllUsers };
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {login, register, me} = require('../controller/auth.controller');
+const {login, register, me, getAllUsers} = require('../controller/auth.controller');
 const { tokenCheck } = require('../middlewares/auth');
 const authValidatin = require('../middlewares/validations/auth.validation')
 
@@ -8,5 +8,7 @@ router.post('/login', authValidatin.login, login)
 router.post('/register', authValidatin.register, register)
 
 router.get('/me', tokenCheck,  me)
+
+router.get('/getAllUser', getAllUsers)
 
 module.exports = router
